09b5f0dd9f2e2674f818597a81680d9486f1a87b
MDementev <vanneste1207@gmail.com>
2022-10-16 09:01:56 +0300

    4 задача

---
+++ zadanie4.py
@@ -0,0 +1,17 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях. Позиции вводятся с клавиатуры .
+
+from random import randint
+
+N = int(input('-> '))
+numbers = []
+for i in range(N):
+    numbers.append(randint(-N, N))
+print(numbers)
+
+x = int(input('first position = '))
+y = int(input('second position = '))
+
+for i in range(len(numbers)):
+    mult = numbers[x-1]*numbers[y-1]
+print(f'Mult of elements: {numbers[x-1]} * {numbers[y-1]} =', mult)
